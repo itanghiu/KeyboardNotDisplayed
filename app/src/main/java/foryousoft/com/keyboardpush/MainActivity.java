7220fc77c6c4137744f79380f907bb7b577bebe2
ihiu <ihiu@docapost.fr>
2016-02-23 18:01:25 +0100

    initial commit

---
+++ app/src/main/java/foryousoft/com/keyboardpush/MainActivity.java
@@ -0,0 +1,17 @@
+package foryousoft.com.keyboardpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
